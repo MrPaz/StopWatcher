b0389feb95a74d29b4b76e2f3d641e998ec17c92
Matt Pazar <mattpazar@yahoo.com>
2019-03-14 10:42:11 -0500

    Add project files.

---
+++ StopWatcher/Controllers/StopWatcherController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StopWatcher.Controllers
+{
+    public class StopWatcherController : Controller
+    {
+        //private XmlDataContext _xmlDataContext;
+
+        //public StopWatcherController(ILogger<StopWatcherController> logger, XmlDataContext xmlDataContext)
+        //{
+        //    _xmlDataContext = xmlDataContext;
+        //    logger.Log(LogLevel.Information, "Shopping List Controller Constructed");
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
